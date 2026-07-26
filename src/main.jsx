b9ae8fa1b561c8348f3aecc57205fef862b875f1
Devansh Kashyap <devansh14007@gmail.com>
2026-07-26 20:11:53 +0530

    Fixed the scroll down bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,26 @@
-import { BrowserRouter } from "react-router-dom";
-import { StrictMode } from 'react'
+import { BrowserRouter, useLocation } from "react-router-dom";
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 import "./styles/fonts.css";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>,
